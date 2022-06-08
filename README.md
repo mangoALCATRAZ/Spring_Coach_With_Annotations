d6103d3095866bfc694a5f2ce4ae9eeba480b013
mangoALCATRAZ <mangoALCATRAZ@github.com>
2022-06-07 23:05:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spring_coach-annotations
---
+++ src/com/luv2code/springdemo/BadmintonCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BadmintonCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "shuttlecock aaaaahahhahahahahahha cock";
+    }
+}
